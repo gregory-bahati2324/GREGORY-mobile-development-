ab8dead095bd86db1c1426eaf2f9a389ab83bc38
gregory-bahati2324 <gregorybahati8@gmail.com>
2024-09-04 16:35:43 +0300

    userfeedback of code

---
+++ userfeedback.java
@@ -0,0 +1,22 @@
+package com.example.registerloginapp;
+
+public class userfeedback {
+    String username, Mrejesho;
+
+    public userfeedback(){
+
+    }
+
+    public userfeedback(String username, String mrejesho) {
+        this.username = username;
+        Mrejesho = mrejesho;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getMrejesho() {
+        return Mrejesho;
+    }
+}
